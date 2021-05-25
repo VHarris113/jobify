66a8b38190c0132a6956aca0bc3413771c63e3d0
Veronica Harris <veronica.n.harris92@gmail.com>
2021-05-25 13:46:18 -0400

    adding login to routes

--- routes/login.js
+++ routes/login.js
@@ -1,1 +1,33 @@
-//login route
+var loginFormHandler = async (event) => {
+    event.preventDefault();
+
+    const email = document.querySelector("#email-login").value.trim();
+    const password = document.querySelector("#password-login").value.trim();
+
+    if (email && password) {
+
+        const response = await fetch("./models/profile", {
+            method: 'POST',
+            body: JSON.stringify({ email, password }),
+            headers: { "Content-Type": "application/json" },
+        });
+
+        if (response.ok) {
+            document.location.replace("/pages/resume");
+        } else {
+            //need code for when login is not correct
+        }
+    }
+};
+
+var signupFormHandler = async (event) => {
+    event.preventDefault();
+
+    const username = document.querySelector("#name-signup").value.trim();
+    const email = document.querySelector("#email-signup").value.trim();
+    const password = document.querySelector("#password-signup").value.trim();
+
+    if (name && email && password) {
+        const response = await fetch("/api/")
+    }
+}
