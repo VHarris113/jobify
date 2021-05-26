41f17fd834b07144cc203c73c6efcfb72285a41d
Mujinga Williams <mujingawilliams@Mujingas-MBP.attlocal.net>
2021-05-26 11:37:14 -0400

    add button

--- client/src/pages/insert.js
+++ client/src/pages/insert.js
@@ -2,105 +2,79 @@ import React from "react";
 import { Link, useParams } from "react-router-dom";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
-import ReactDOM from 'react-dom';
+import ReactDOM from "react-dom";
 import { Form } from "react-bootstrap";
 import { InputGroup } from "react-bootstrap";
-import {FormControl} from "react-bootstrap";
-import {Button} from "react-bootstrap";
+import { FormControl } from "react-bootstrap";
+import { Button } from "react-bootstrap";
 import Navbar from "react-bootstrap/Navbar";
-import {Badge} from "react-bootstrap";
-
+import { Badge } from "react-bootstrap";
 
 function insert() {
-    return (
-     
-      <Container>
-      
-        <div className="row">
-      <div className="badge col md-1">
-                <button  id= "wishlist-insert" style={{ width: 120, height: 120 }} type="button" class="btn ">
-                Wishlist<span class="badge badge-secondary"> 1</span>
-                </button>
-            </div>
-            <div className="badge col md-1">
-                <button id= "pending-insert"   style={{ width: 120, height: 120 }} type="button" class="btn ">
-                  Pending <span class="badge badge-secondary"> 3</span>
-                </button>
-            </div>
-            <div  className="badge col md-1">
-                <button  id= "denied-insert"  style={{ width: 120, height:120 }} type="button" class="btn ">
-                  Denied <span class="badge badge-secondary"> 3</span>
-                </button>
-            </div>
-            <div className="badge col md-1">
-                <button  id= "offer-insert"   style={{ width: 120, height: 120 }} type="button" class="btn ">
-                  Offer <span class="badge badge-secondary"> 3</span>
-                </button>
-            </div>
-            </div>
-         
-            <div style={{ display: 'block', 
-                  width: 700, 
-                  padding: 30 }}>
-      <h4>Select your status and enter the information requested below</h4>
-      <Form>
-      <Form.Group>
-          <Form.Label></Form.Label>
-          <Form.Control type="text" 
-                        placeholder="Company:" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label></Form.Label>
-          <Form.Control type="text" 
-                        placeholder="Job Title" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label></Form.Label>
-          <Form.Control type="text" placeholder="Location" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label></Form.Label>
-          <Form.Control type="number" 
-                        placeholder="Salary" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label></Form.Label>
-          <Form.Control type="text" placeholder="Listing URL" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label></Form.Label>
-          <Form.Control type="text" 
-                        placeholder="Date Applied" />
-        </Form.Group>
-        <Form.Group>
-          <Form.Label></Form.Label>
-          <Form.Control type="text" placeholder="Follow Up Date" />
-        </Form.Group>
-      
-  <Form.Group controlId="exampleForm.ControlTextarea1">
-    <Form.Label></Form.Label>
-    <Form.Control as="textarea" rows={3} placeholder="Notes"/>
-  </Form.Group>
-</Form>
+  return (
+    <Container>
+       <div style={{ display: "block", width: 700, padding: 30 }}>
+       <h4 >Select your status:</h4>
+       </div>
+       <div style={{ display: "block", width: 700, padding: 30 }}>
+      <Button variant="outline-primary">Wishlist</Button>{" "}
+      <Button variant="outline-secondary">Pending</Button>{" "}
+      <Button variant="outline-success">Denied</Button>{" "}
+      <Button variant="outline-warning">Offer</Button>{" "}
+      </div>
+      <div style={{ display: "block", width: 700, padding: 30 }}>
+       <h4> Enter the information requested below:</h4>
+       </div>
+      <div style={{ display: "block", width: 700, padding: 30 }}>
+    
+        <Form>
+          <Form.Group>
+            <Form.Label></Form.Label>
+            <Form.Control type="text" placeholder="Company:" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label></Form.Label>
+            <Form.Control type="text" placeholder="Job Title" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label></Form.Label>
+            <Form.Control type="text" placeholder="Location" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label></Form.Label>
+            <Form.Control type="number" placeholder="Salary" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label></Form.Label>
+            <Form.Control type="text" placeholder="Listing URL" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label></Form.Label>
+            <Form.Control type="text" placeholder="Date Applied" />
+          </Form.Group>
+          <Form.Group>
+            <Form.Label></Form.Label>
+            <Form.Control type="text" placeholder="Follow Up Date" />
+          </Form.Group>
 
-<Button variant="primary" type="submit">
-Click here to submit form
-</Button>
+          <Form.Group controlId="exampleForm.ControlTextarea1">
+            <Form.Label></Form.Label>
+            <Form.Control as="textarea" rows={3} placeholder="Notes" />
+          </Form.Group>
+        </Form>
 
-</div>
-
-
-{/* <div className="container">
+        <Button variant="primary" type="submit">
+          Click here to submit form
+        </Button>
+      </div>
+      {/* <div className="container">
 <div class="form-floating">
 <textarea class="form-control" placeholder="Leave a comment here" id="floatingTextarea"></textarea>
 <label for="floatingTextarea">Comments</label>
 </div>
   </div>          */}
-  
-</Container> 
+    </Container>
+  );
+}
 
-
-        );
-    }   
-    
-    export default insert;
+export default insert;
