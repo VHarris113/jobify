c199a74da7f8e17bba5e855eef7f0a0c498c792a
Jackson Peak <jacksonpeak1@icloud.com>
2021-05-26 12:00:54 -0400

    login signup front end

--- client/src/pages/login.js
+++ client/src/pages/login.js
@@ -1,50 +1,118 @@
 import React from "react";
 import Jumbotron from "../components/Jumbotron";
 import { Col, Row, Container } from "../components/Grid";
+
 function login() {
-    return (
-        <section class="sign-in">
-            <div class="container">
-                <div class="signin-content">
-                    <div class="signin-image">
-                        <figure><img src="images/signin-image.jpg" alt="sing up image"/></figure>
-                        <a href="#" class="signup-image-link">Create an account</a>
-                    </div>
+  var axios = require("axios");
+  var data = JSON.stringify({
+    email: "",
+    password: "",
+  });
 
-                    <div class="signin-form">
-                        <h2 class="form-title">Sign up</h2>
-                        <form method="POST" class="register-form" id="login-form">
-                            <div class="form-group">
-                                <label for="your_name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="your_name" id="your_name" placeholder="Your Name"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="your_pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="your_pass" id="your_pass" placeholder="Password"/>
-                            </div>
-                            <div class="form-group">
-                                <input type="checkbox" name="remember-me" id="remember-me" class="agree-term" />
-                                <label for="remember-me" class="label-agree-term"><span><span></span></span>Remember me</label>
-                            </div>
-                            <div class="form-group form-button">
-                                <input type="submit" name="signin" id="signin" class="form-submit" value="Log in"/>
-                            </div>
-                        </form>
-                        <div class="social-login">
-                            <span class="social-label">Or login with</span>
-                            <ul class="socials">
-                                <li><a href="#"><i class="display-flex-center zmdi zmdi-facebook"></i></a></li>
-                                <li><a href="#"><i class="display-flex-center zmdi zmdi-twitter"></i></a></li>
-                                <li><a href="#"><i class="display-flex-center zmdi zmdi-google"></i></a></li>
-                            </ul>
-                        </div>
-                    </div>
-                </div>
+  var config = {
+    method: "post",
+    url: "http://localhost:3001/api/login",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: "",
+    },
+    data: data,
+  };
+
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return (
+    <section class="sign-in">
+      <div class="container">
+        <div class="signin-content">
+          <div class="signin-image">
+            <figure>
+              <img src="images/signin-image.jpg" alt="sing up image" />
+            </figure>
+            <a href="#" class="signup-image-link">
+              Create an account
+            </a>
+          </div>
+
+          <div class="signin-form">
+            <h2 class="form-title">Sign up</h2>
+            <form method="POST" class="register-form" id="login-form">
+              <div class="form-group">
+                <label for="your_name">
+                  <i class="zmdi zmdi-account material-icons-name"></i>
+                </label>
+                <input
+                  type="text"
+                  name="your_name"
+                  id="your_name"
+                  placeholder="Your Name"
+                />
+              </div>
+              <div class="form-group">
+                <label for="your_pass">
+                  <i class="zmdi zmdi-lock"></i>
+                </label>
+                <input
+                  type="password"
+                  name="your_pass"
+                  id="your_pass"
+                  placeholder="Password"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  type="checkbox"
+                  name="remember-me"
+                  id="remember-me"
+                  class="agree-term"
+                />
+                <label for="remember-me" class="label-agree-term">
+                  <span>
+                    <span></span>
+                  </span>
+                  Remember me
+                </label>
+              </div>
+              <div class="form-group form-button">
+                <input
+                  type="submit"
+                  name="signin"
+                  id="signin"
+                  class="form-submit"
+                  value="Log in"
+                />
+              </div>
+            </form>
+            <div class="social-login">
+              <span class="social-label">Or login with</span>
+              <ul class="socials">
+                <li>
+                  <a href="#">
+                    <i class="display-flex-center zmdi zmdi-facebook"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="display-flex-center zmdi zmdi-twitter"></i>
+                  </a>
+                </li>
+                <li>
+                  <a href="#">
+                    <i class="display-flex-center zmdi zmdi-google"></i>
+                  </a>
+                </li>
+              </ul>
             </div>
-        </section>
-);
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
-
-
 
 export default login;
--- client/src/pages/signup.js
+++ client/src/pages/signup.js
@@ -1,51 +1,122 @@
 import React from "react";
 import { Col, Row, Container } from "../components/Grid";
 import Jumbotron from "../components/Jumbotron";
+
 function login() {
-    return (
-        <section class="signup">
-            <div class="container">
-                <div class="signup-content">
-                    <div class="signup-form">
-                        <h2 class="form-title">Sign up</h2>
-                        <form method="POST" class="register-form" id="register-form">
-                            <div class="form-group">
-                                <label for="name"><i class="zmdi zmdi-account material-icons-name"></i></label>
-                                <input type="text" name="name" id="name" placeholder="Your Name"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="email"><i class="zmdi zmdi-email"></i></label>
-                                <input type="email" name="email" id="email" placeholder="Your Email"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="pass"><i class="zmdi zmdi-lock"></i></label>
-                                <input type="password" name="pass" id="pass" placeholder="Password"/>
-                            </div>
-                            <div class="form-group">
-                                <label for="re-pass"><i class="zmdi zmdi-lock-outline"></i></label>
-                                <input type="password" name="re_pass" id="re_pass" placeholder="Repeat your password"/>
-                            </div>
-                            <div class="form-group">
-                                <input type="checkbox" name="agree-term" id="agree-term" class="agree-term" />
-                                <label for="agree-term" class="label-agree-term"><span><span></span></span>I agree all statements in  <a href="#" class="term-service">Terms of service</a></label>
-                            </div>
-                            <div class="form-group form-button">
-                                <input type="submit" name="signup" id="signup" class="form-submit" value="Register"/>
-                            </div>
-                        </form>
-                    </div>
-                    <div class="signup-image">
-                        <figure><img src="images/signup-image.jpg" alt="sing up image"/></figure>
-                        <a href="#" class="signup-image-link">I am already member</a>
-                    </div>
-                </div>
-            </div>
-        </section>
+  var axios = require("axios");
+  var data = JSON.stringify({
+    email: "",
+    password: "",
+  });
 
+  var config = {
+    method: "post",
+    url: "http://localhost:3001/api/signup",
+    headers: {
+      "Content-Type": "application/json",
+      Cookie: "",
+    },
+    data: data,
+  };
 
-);
+  axios(config)
+    .then(function (response) {
+      console.log(JSON.stringify(response.data));
+    })
+    .catch(function (error) {
+      console.log(error);
+    });
+  return (
+    <section class="signup">
+      <div class="container">
+        <div class="signup-content">
+          <div class="signup-form">
+            <h2 class="form-title">Sign up</h2>
+            <form method="POST" class="register-form" id="register-form">
+              <div class="form-group">
+                <label for="name">
+                  <i class="zmdi zmdi-account material-icons-name"></i>
+                </label>
+                <input
+                  type="text"
+                  name="name"
+                  id="name"
+                  placeholder="Your Name"
+                />
+              </div>
+              <div class="form-group">
+                <label for="email">
+                  <i class="zmdi zmdi-email"></i>
+                </label>
+                <input
+                  type="email"
+                  name="email"
+                  id="email"
+                  placeholder="Your Email"
+                />
+              </div>
+              <div class="form-group">
+                <label for="pass">
+                  <i class="zmdi zmdi-lock"></i>
+                </label>
+                <input
+                  type="password"
+                  name="pass"
+                  id="pass"
+                  placeholder="Password"
+                />
+              </div>
+              <div class="form-group">
+                <label for="re-pass">
+                  <i class="zmdi zmdi-lock-outline"></i>
+                </label>
+                <input
+                  type="password"
+                  name="re_pass"
+                  id="re_pass"
+                  placeholder="Repeat your password"
+                />
+              </div>
+              <div class="form-group">
+                <input
+                  type="checkbox"
+                  name="agree-term"
+                  id="agree-term"
+                  class="agree-term"
+                />
+                <label for="agree-term" class="label-agree-term">
+                  <span>
+                    <span></span>
+                  </span>
+                  I agree all statements in{" "}
+                  <a href="#" class="term-service">
+                    Terms of service
+                  </a>
+                </label>
+              </div>
+              <div class="form-group form-button">
+                <input
+                  type="submit"
+                  name="signup"
+                  id="signup"
+                  class="form-submit"
+                  value="Register"
+                />
+              </div>
+            </form>
+          </div>
+          <div class="signup-image">
+            <figure>
+              <img src="images/signup-image.jpg" alt="sing up image" />
+            </figure>
+            <a href="#" class="signup-image-link">
+              I am already member
+            </a>
+          </div>
+        </div>
+      </div>
+    </section>
+  );
 }
-
-
 
 export default login;
