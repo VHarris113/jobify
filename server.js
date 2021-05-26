abdb9cb73414bcfbfd3af46a2468204823edf052
Jackson Peak <jacksonpeak1@icloud.com>
2021-05-25 23:01:04 -0400

    added sign up and log in

--- server.js
+++ server.js
@@ -16,9 +16,9 @@ app.use(express.urlencoded({ limit: "50mb", extended: true }));
 app.use(express.json({ limit: "50mb" }));
 app.use(cors());
 // Serve up static assets (usually on heroku)
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static("client/build"));
-// }
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
 
 // Define API routes here
 
