2a7d644a3d7cb8234ce4fcda5a2327f3775b998b
Mujinga Williams <mujingawilliams@Mujingas-MBP.attlocal.net>
2021-05-27 22:35:32 -0400

    added email

--- client/src/components/cron.js
+++ client/src/components/cron.js
@@ -1,28 +1,54 @@
-import React, { Component } from 'react'
-import Cron from 'react-cron-generator'
-import 'react-cron-generator/dist/cron-builder.css'
+// 
+// const express = require('express');
+// const fs = require('fs'); // previous example
+// const shell = require('shelljs'); // previous example
+const nodemailer = require('nodemailer');
 
 
-class App extends Component {
+function sendMail (){
+// TODO
+// make a request to your app's backend
+//     grab all users
+//     save users to an array
+// loop over array
+//     for each user construct new email & send to them
 
-  constructor(props) {
-      super(props);
-      this.state = {
-       
-      };
-  }
+// Create mail transporter.
+let transporter = nodemailer.createTransport({
+    // host: 'hotmail.com',
+    // port:  465,
+    service: "hotmail",
+    // secure: true,
+    auth: {
+        user: "(enter sender email)",
+        pass: "(enter sender password)"
+    }
+  });
+  
+//   app.listen(3000);
+  // ...
 
-  render() {
-    return (<div>
-      <Cron
-        onChange={(e)=> {this.setState({value:e});}}
-        value={this.state.value}
-        showResultText={true}
-        showResultCron={true}
-        />
-                            
-    </div>)
-  }
+// Sending emails every Wednesday.
+// cron.schedule('0 0 * * 3', function() {
+//     console.log('---------------------');
+//     console.log('Running Cron Job');
+  
+    let messageOptions = {
+      from: '(sender email)',
+      to: '(recipient)',
+      subject: 'Scheduled Email',
+      text: "Hello ${username},You have selected a reminder for every ${time.frame} to notify you follow up on your career path with ${job.name}. Please sign into your account here ${jobify.link} to get more details. Good luck to you!Sincerely,Your Jobify Team"
+    };
+  
+    transporter.sendMail(messageOptions, function(error, info) {
+      if (error) {
+        throw error;
+      } else {
+        console.log('Email successfully sent!');
+      }
+    });
+
+//   });
 }
-
-export default App;
+//   app.listen(3000);
+export default{sendMail};
