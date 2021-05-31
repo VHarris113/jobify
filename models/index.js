22ef6d6837efa7f241b8408affa3129f9136b9e5
Eloy Gonzalez <eloy522752@gmail.com>
2021-05-31 10:08:48 -0400

    fixed issue with modlels

--- models/index.js
+++ models/index.js
@@ -1,6 +1,6 @@
 const User = require("./user");
 const Resume = require("./resume");
-const Form = require("./form");
+const Form = require("./Form");
 const Signup = require("./signup");
 
 module.exports = { User, Resume, Form, Signup };
